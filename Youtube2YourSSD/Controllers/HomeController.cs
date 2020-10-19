117fb597f180da33a88350f27cc9d52f7806b660
matveivitalie <matveivitalie@gmail.com>
2020-10-19 12:18:07 +0300

    Added FORM on client side

--- Youtube2YourSSD/Controllers/HomeController.cs
+++ Youtube2YourSSD/Controllers/HomeController.cs
@@ -11,27 +11,31 @@ namespace Youtube2YourSSD.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public List<YouTubeVideoQuality> YouTubeDownloader(params string[] VideoUrls) 
         {
-            _logger = logger;
+        
         }
+        /* private readonly ILogger<HomeController> _logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+         public HomeController(ILogger<HomeController> logger)
+         {
+             _logger = logger;
+         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+         public IActionResult Index()
+         {
+             return View();
+         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+         public IActionResult Privacy()
+         {
+             return View();
+         }
+
+         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+         public IActionResult Error()
+         {
+             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+         }*/
     }
 }
---
+++ Youtube2YourSSD/Models/YouTubeVideoQuality.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Youtube2YourSSD.Models
+{
+    public class YouTubeVideoQuality
+    {
+        public string VideoTitle { get; set; }
+        public string Extension { get; set; }
+        public string DownloadUrl { get; set; }
+        public string VideoUrl { get; set; }
+        public Size Dimension { get; set; }
+
+        public override string ToString()
+        {
+            return Extension + " File " + Dimension.Width +
+                                    "x" + Dimension.Height;
+        }
+
+        public void SetQuality(string  Extension, Size Dimension)
+        {
+            this.Extension = Extension;
+            this.Dimension = Dimension;
+        }
+    }
+}
